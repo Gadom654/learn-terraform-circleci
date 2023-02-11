33d3a07ec6e2fb2966c24cd909fdf8e51cfa8846
Gadom654 <dominikgadomski654@gmail.com>
2023-02-11 14:21:00 +0100

    m

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "cluster_nodes" {
-  name       = aws_instance.flask.tags["terraform-kubeadm:node"]
-  subnet_id  = aws_instance.flask.subnet_id
-  private_ip = aws_instance.flask.private_ip
-  public_ip  = aws_instance.flask.tags["terraform-kubeadm:node"] == "apache" ? aws_eip.apache.public_ip : i.public_ip
-  description = "Name, public and private IP address, and subnet ID of all nodes of the created cluster."
-}
-
 output "vpc_id" {
   value       = aws_security_group.egress.vpc_id
   description = "ID of the VPC in which the cluster has been created."
