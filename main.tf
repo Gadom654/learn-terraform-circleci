67d82dee83434ae7e4ed4ca8136b93135bb7488c
Gadom654 <dominikgadomski654@gmail.com>
2023-04-06 11:11:06 +0200

    test

--- main.tf
+++ main.tf
@@ -17,6 +17,15 @@ resource "aws_subnet" "web_subnet" {
   vpc_id     = aws_vpc.vpc.id
 }
 
+resource "aws_route_table" "vpc_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
 # Definiujemy grupę bezpieczeństwa
 resource "aws_security_group" "web" {
   name_prefix = "web_"
