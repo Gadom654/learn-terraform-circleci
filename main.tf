05b80e7877cd6a9b7e9416b38daa7827d15f4a71
Gadom654 <dominikgadomski654@gmail.com>
2023-04-06 08:50:44 +0200

    rebuild

--- main.tf
+++ main.tf
@@ -1,262 +1,40 @@
-terraform {
-  required_version = ">= 0.12"
-}
-provider "aws" {
-  region = "eu-central-1"
-}
-#------------------------------------------------------------------------------#
-# Common local values
-#------------------------------------------------------------------------------#
-#
-locals {
-  tags = merge(var.tags, { "terraform-kubeadm:cluster" = var.cluster_name })
-}
+resource "aws_instance" "flask" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  key_name      = "my-key"
+  vpc_security_group_ids = [aws_security_group.flask.id]
 
-#------------------------------------------------------------------------------#
-# Key pair
-#------------------------------------------------------------------------------#
+  subnet_id = aws_subnet.public.id
 
-# Performs 'ImportKeyPair' API operation (not 'CreateKeyPair')
-resource "aws_key_pair" "main" {
-  key_name_prefix = "${var.cluster_name}"
-  public_key      = file(var.public_key_file)
-  tags            = local.tags
-}
-#------------------------------------------------------------------------------#
-# Network
-#------------------------------------------------------------------------------#
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-  tags       = var.tags
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags   = var.tags
-}
-
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.cidr_block
-  tags       = var.tags
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo yum -y install python3-pip",
+      "sudo pip3 install flask",
+    ]
   }
-  tags = var.tags
-}
 
-resource "aws_route_table_association" "main" {
-  route_table_id = aws_route_table.main.id
-  subnet_id      = aws_subnet.main.id
-}
-#------------------------------------------------------------------------------#
-# Security groups
-#------------------------------------------------------------------------------#
-
-# The AWS provider removes the default "allow all "egress rule from all security
-# groups, so it has to be defined explicitly.
-resource "aws_security_group" "egress" {
-  name        = "${var.cluster_name}-egress"
-  description = "Allow all outgoing traffic to everywhere"
-  vpc_id      = aws_vpc.main.id
-  tags        = local.tags
-  egress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ingress_internal" {
-  name        = "${var.cluster_name}-ingress-internal"
-  description = "Allow all incoming traffic from nodes and Pods in the cluster"
-  vpc_id      = aws_vpc.main.id
-  tags        = local.tags
-  ingress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    self        = true
-    description = "Allow incoming traffic from cluster nodes"
-
-  }
-  ingress {
-    protocol    = -1
-    from_port   = 0
-    to_port     = 0
-    cidr_blocks = var.pod_network_cidr_block != null ? [var.pod_network_cidr_block] : null
-    description = "Allow incoming traffic from the Pods of the cluster"
-  }
-}
-
-resource "aws_security_group" "ingress_k8s" {
-  name        = "${var.cluster_name}-ingress-k8s"
-  description = "Allow incoming Kubernetes API requests (TCP/6443) from outside the cluster"
-  vpc_id      = aws_vpc.main.id
-  tags        = local.tags
-  ingress {
-    protocol    = "tcp"
-    from_port   = 6443
-    to_port     = 6443
-    cidr_blocks = var.allowed_k8s_cidr_blocks
-  }
-}
-
-resource "aws_security_group" "ingress_ssh" {
-  name        = "${var.cluster_name}-ingress-ssh"
-  description = "Allow incoming SSH traffic (TCP/22) from outside the cluster"
-  vpc_id      = aws_vpc.main.id
-  tags        = local.tags
-  ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = var.allowed_ssh_cidr_blocks
-  }
-}
-
-#------------------------------------------------------------------------------#
-# Elastic IP for apache node
-#------------------------------------------------------------------------------#
-
-#EIP for apache node because it must know its public IP during initialisation
-resource "aws_eip" "apache" {
-  vpc  = true
-  tags = local.tags
-}
-
-resource "aws_eip_association" "apache" {
-  allocation_id = aws_eip.apache.id
-  instance_id   = aws_instance.apache.id
-}
-
-#------------------------------------------------------------------------------#
-# Bootstrap token for kubeadm
-#------------------------------------------------------------------------------#
-
-# Generate bootstrap token
-# See https://kubernetes.io/docs/reference/access-authn-authz/bootstrap-tokens/
-resource "random_string" "token_id" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "random_string" "token_secret" {
-  length  = 16
-  special = false
-  upper   = false
-}
-
-locals {
-  token = "${random_string.token_id.result}.${random_string.token_secret.result}"
-}
-
-#------------------------------------------------------------------------------#
-# EC2 instances
-#------------------------------------------------------------------------------#
-
-data "aws_ami" "ubuntu" {
-  # AMI owner ID of Canonical
-  owners      = ["099720109477"] 
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  tags = {
+    Name = "flask"
   }
 }
 
 resource "aws_instance" "apache" {
-  ami           = data.aws_ami.ubuntu.image_id
-  instance_type = var.apache_instance_type
-  subnet_id     = aws_subnet.main.id
-  key_name      = aws_key_pair.main.key_name
-  vpc_security_group_ids = [
-    aws_security_group.egress.id,
-    aws_security_group.ingress_internal.id,
-    aws_security_group.ingress_k8s.id,
-    aws_security_group.ingress_ssh.id
-  ]
-  tags      = merge(local.tags, { "terraform-kubeadm:node" = "apache" })
-  # Saved in: /var/lib/cloud/instances/<instance-id>/user-data.txt [1]
-  # Logs in:  /var/log/cloud-init-output.log [2]
-  # [1] https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html#user-data-shell-scripts
-  # [2] https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html#user-data-shell-scripts
-  user_data = templatefile(
-    "${path.module}/user-data.tftpl",
-    {
-      node              = "apache",
-      token             = local.token,
-      cidr              = var.pod_network_cidr_block
-      apache_public_ip  = aws_eip.apache.public_ip,
-      apache_private_ip = null,
-    }
-  )
-}
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  key_name      = "my-key"
+  vpc_security_group_ids = [aws_security_group.apache.id]
 
-resource "aws_instance" "flask" {
-  ami                         = data.aws_ami.ubuntu.image_id
-  instance_type               = var.flask_instance_type
-  subnet_id                   = aws_subnet.main.id
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.main.key_name
-  vpc_security_group_ids = [
-    aws_security_group.egress.id,
-    aws_security_group.ingress_internal.id,
-    aws_security_group.ingress_ssh.id
-  ]
-  tags      = merge(local.tags, { "terraform-kubeadm:node" = "flask" })
-  user_data = templatefile(
-    "${path.module}/user-data.tftpl",
-    {
-      node              = "flask",
-      token             = local.token,
-      cidr              = null,
-      apache_public_ip  = null,
-      apache_private_ip = aws_instance.apache.private_ip,
-    }
-  )
-}
+  subnet_id = aws_subnet.public.id
 
-#------------------------------------------------------------------------------#
-# Wait for bootstrap to finish on all nodes
-#------------------------------------------------------------------------------#
-
-resource "null_resource" "wait_for_bootstrap_to_finish" {
-  provisioner "local-exec" {
-    command = <<-EOF
-    alias ssh='ssh -q -i ${var.private_key_file} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-    while true; do
-      sleep 2
-      ! ssh ubuntu@${aws_eip.apache.public_ip} [[ -f /home/ubuntu/done ]] >/dev/null && continue
-      ! ssh ubuntu@${aws_instance.flask.public_ip} [[ -f /home/ubuntu/done ]] >/dev/null && continue
-      break
-    done
-    EOF
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo yum -y install httpd",
+    ]
   }
-  triggers = {
-    instance_ids = join(",",[aws_instance.apache.id, aws_instance.flask.id])
+
+  tags = {
+    Name = "apache"
   }
 }
-
-#------------------------------------------------------------------------------#
-# Download kubeconfig file from apache node to local machine
-#------------------------------------------------------------------------------#
-# 
-# resource "null_resource" "download_kubeconfig_file" {
-#   provisioner "local-exec" {
-#     command = <<-EOF
-#     alias scp='scp -q -i ${var.private_key_file} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
-#     scp ubuntu@${aws_eip.apache.public_ip}:/home/ubuntu/admin.conf ${var.kubeconfig != null ? var.kubeconfig : "${var.cluster_name}.conf"} >/dev/null
-#     EOF
-#   }
-#   triggers = {
-#     wait_for_bootstrap_to_finish = null_resource.wait_for_bootstrap_to_finish.id
-#   }
-# }
